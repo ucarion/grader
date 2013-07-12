b91b2e2400cf348608ef6314365b29ade74c0240
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-11 22:41:09 -0700

    Initial commit
    Basically just rails new, but with some modifications to the .gitignore
    and Gemfile.

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0'
+
+# Use sqlite3 as the database for Active Record
+# Remember to use 'pg' for deployment on Heroku
+group :development do
+  gem 'sqlite3'
+end
+
+group :assets do
+  # Use SCSS for stylesheets
+  gem 'sass-rails', '~> 4.0.0'
+
+  # Use Uglifier as compressor for JavaScript assets
+  gem 'uglifier', '>= 1.3.0'
+
+  # Use CoffeeScript for .js.coffee assets and views
+  gem 'coffee-rails', '~> 4.0.0'
+end
+
+# Use jquery as the JavaScript library
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grader::Application.config.secret_key_base = '65a737ef6a8c8ca3d52054b3f78a1f54ef074a84d1a0279ce99652209bef136e77495c8fe68d4fcc636be59e58f1a98acac56bdb34cefc9cd5bda3c230b2f16a'
