7d30910b1bdf4140ab28a17f9b87730f5951c461
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-12 14:40:51 -0700

    Test for changing titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,11 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Grader')
     end
+
+    it "should have 'Home' in the title" do
+      visit '/static_pages/home'
+      expect(page).to have_title('Grader | Home')
+    end
   end
 
   describe "Help page" do
@@ -13,12 +18,22 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have 'Help' in the title" do
+      visit '/static_pages/help'
+      expect(page).to have_title('Grader | Help')
+    end
   end
 
   describe "About page" do
     it "should have the content 'About'" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
+    end
+
+    it "should have 'About' in the title" do
+      visit '/static_pages/about'
+      expect(page).to have_title('Grader | About')
     end
   end
 end
