275dd17cf850b83d14a062615b698457d6db5799
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-13 11:08:50 -0700

    Test for named path variables

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,36 +3,36 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Grader'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Grader')
     end
 
     it "should have 'Home' in the title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title('Grader')
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have 'Help' in the title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title('Grader | Help')
     end
   end
 
   describe "About page" do
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About')
     end
 
     it "should have 'About' in the title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title('Grader | About')
     end
   end
