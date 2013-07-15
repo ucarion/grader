d4a84b733f1e35f5fd1d404e8a1de955933753dc
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-14 17:54:46 -0700

    Validate for email uniqueness (case-insenstive)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name, presence: true
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
