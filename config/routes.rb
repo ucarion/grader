cb84c7c4ca7414be2c3683e523c35d0678f63d01
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-14 19:48:11 -0700

    Add resources for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Grader::Application.routes.draw do
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
-  
+
+  resources :users  
   match '/signup', to: 'users#new', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
