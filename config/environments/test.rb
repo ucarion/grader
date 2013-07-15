b877b3734880f9402027ad54daa300d428f5a26c
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-14 22:36:42 -0700

    Make bcrypt be as fast as possible when in test mode

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ Grader::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Use faster hashing when in test mode
+  ActiveModel::SecurePassword.min_cost = true
 end
