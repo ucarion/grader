f96294156c80ea5db96c18b342c431025a9c9fe6
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-14 19:07:19 -0700

    Validate for User passwords at least 6 characters long

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ActiveRecord::Base
   
   validates :name, presence: true
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
 end
