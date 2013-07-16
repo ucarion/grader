7c4f1cc851d0513141ada049221e2c917fe0944c
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-16 12:59:31 -0700

    Add named routes and resources for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Grader::Application.routes.draw do
   resources :users  
   match '/signup', to: 'users#new', via: 'get'
 
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
