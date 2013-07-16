09754c6c1755e90279fe76e7b1018b3d7dd32854
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-16 14:39:03 -0700

    Add a sign_out method and have sessions#destroy call it

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,11 @@ module SessionsHelper
     self.current_user = current_user
   end
 
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
+
   def signed_in?
     current_user
   end
