dcb7951c941aa0f67d8d9b34cf52cd5fefb5bab2
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-16 14:24:24 -0700

    Migrate DB and add functionality for keeping users signed in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "Welcome back, #{user.name}!"
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = "Invalid email/password combination"
+      render 'new'
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,25 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { email.downcase! }
+  before_create :create_remember_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name, presence: true
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end
