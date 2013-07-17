62d5bf7e8ba0e6876bf3c317c4fdd86ed60f8d7b
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-17 10:12:21 -0700

    Add edit and update methods for Users
    The update method does not yet handle successful updates yet.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      # todo
+    else
+      render 'new'
+    end
+  end
+
   private
 
   def user_params
