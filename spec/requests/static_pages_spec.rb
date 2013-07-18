4dab65d821d88e2b954088e767f57aeb7800d4b8
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-18 08:59:40 -0700

    For good measure, add tests for the header when logged in or out

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -23,4 +23,22 @@ describe "Static pages" do
     it { should have_content('About') }
     it { should have_title('Grader | About') }
   end
+
+  describe "Header" do
+    before { visit root_path }
+
+    describe "when not logged in" do
+      it { should have_link "Sign in", href: signin_path }
+    end
+
+    describe "when logged in" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      before { sign_in(user) }
+
+      it { should have_link "Profile", href: user_path(user) }
+      it { should have_link "Settings", href: edit_user_path(user) }
+      it { should have_link "Sign out", href: signout_path }
+    end
+  end
 end
