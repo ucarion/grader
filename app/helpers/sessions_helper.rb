229317dcb7120f99ba77120aede6f57425a70bea
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-18 10:49:02 -0700

    Add a current_user? method to SessionsHelper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -23,4 +23,8 @@ module SessionsHelper
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
   end
+
+  def current_user?(user)
+    @current_user == user
+  end
 end
