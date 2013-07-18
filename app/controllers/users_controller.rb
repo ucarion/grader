4e47371cac5100cd3c4d4e9da7ce16cbfd19e257
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-18 10:50:25 -0700

    Remove pointless initializations of @user
    The check_right_user method now does this for calls to edit and update.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,12 +23,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update_attributes(user_params)
       flash[:success] = "Successfully updated your account information."
       sign_in @user
