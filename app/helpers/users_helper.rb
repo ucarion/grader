b15dfd2f8b2921009a036e55692a9dc6189deae1
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-21 00:09:18 -0700

    Add option to pass a CSS class to gravatar_for

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,6 @@ module UsersHelper
   def gravatar_for(user, args = {})
     id = Digest::MD5::hexdigest(user.email.downcase)
     url = "https://secure.gravatar.com/avatar/#{id}?s=#{args[:size] || 200}"
-    image_tag(url, alt: user.name, class: "gravatar")
+    image_tag(url, alt: user.name, class: "gravatar " + args[:class])
   end
 end
