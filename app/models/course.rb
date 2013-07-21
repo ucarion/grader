cc871cb83d456da08d1d237be0989a8bd27c8352
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-20 21:45:08 -0700

    Have courses validate for the presence of teacher_id and name

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ActiveRecord::Base
   belongs_to :teacher, class_name: "User"
   has_and_belongs_to_many :students, class_name: "User"
+
+  validates :teacher_id, presence: true
+  validates :name, presence: true
 end
