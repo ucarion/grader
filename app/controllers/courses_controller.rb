246e4993d41d1d34ce4daee03c8542eac5be366a
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-25 19:16:01 -0700

    Add new and create actions to the CoursesController

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,4 +2,24 @@ class CoursesController < ApplicationController
   def show
     @course = Course.find(params[:id])
   end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = current_user.taught_courses.build(course_params)
+    if @course.save
+      flash[:success] = "Course #{@course.name} created successfully!"
+      redirect_to @course
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :description)
+  end
 end
