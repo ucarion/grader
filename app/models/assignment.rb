bdfaf2d8c5045412e070f8118fa03c08fcf68cf7
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-26 16:31:02 -0700

    Validate for the presence of assignment name, description, and course_id

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,7 @@
 class Assignment < ActiveRecord::Base
   belongs_to :course
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :course_id, presence: true
 end
