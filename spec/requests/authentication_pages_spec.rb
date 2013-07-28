a2d0fb3d3dabc28965c9ac0e25ceb00688a76c48
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-28 11:43:57 -0700

    Revert "Remove misnamed controller"
    This reverts commit e04bafe42e6487caaf4bff6587114668aedf500b.

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,117 @@
+require 'spec_helper'
+
+describe "Authentication Pages" do
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
+  end
+
+  describe "sign in" do
+    before { visit signin_path }
+
+    describe "with bad information" do
+      before { click_button 'Sign in' }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    end
+
+    describe "with good information" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      before do
+        fill_in "Email", with: user.email
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
+    end
+  end
+
+  describe "authorization" do
+    describe "for actions requiring signin" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:other_user) { FactoryGirl.create(:user, name: "John Doe", email: "john@doe.com") }
+
+      describe "when trying to edit a user" do
+        before { visit edit_user_path(user) }
+
+        it { should have_title('Sign in') }
+
+        describe "after logging in" do
+          before { sign_in(user) }
+
+          it { should have_title('Edit Account') }
+        end
+      end
+
+      describe "when trying to create a course" do
+        before { visit new_course_path }
+
+        it { should have_title('Sign in') }
+      end
+
+      describe "when viewing a course" do
+        let!(:course) { FactoryGirl.create(:course, teacher: user) }
+
+        it { should_not have_selector('.btn-enroll') }
+      end
+
+      describe "when editing a course" do
+        let!(:course) { FactoryGirl.create(:course, teacher: user) }
+
+        before { visit edit_course_path(course) }
+
+        it { should have_title('Sign in') }        
+      end
+
+      describe "as the correct user" do
+        before { sign_in(user) }
+
+        describe "visiting another person's edit page" do
+          before { visit edit_user_path(other_user) }
+
+          it { should_not have_title('Edit Account') }
+        end
+
+        describe "visiting another person's profile page" do
+          before { visit user_path(other_user) }
+
+          it { should have_title(other_user.name) }
+        end
+
+        describe "editing another person's course" do
+          let!(:course) { FactoryGirl.create(:course, teacher: other_user) }
+          
+          before { visit edit_course_path(course) }
+
+          it { should_not have_title('Edit Course') }
+        end
+      end
+
+      describe "as a non-admin" do
+        before { sign_in(user, no_capybara: true ) }
+
+        describe "submitting a DELETE request to Users#destroy" do
+          before { delete user_path(other_user) }
+
+          specify { expect(response).to redirect_to(root_path) }
+        end
+      end
+    end
+  end
+end
