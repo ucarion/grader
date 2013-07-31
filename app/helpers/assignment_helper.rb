8812c53055e6aec1d3bc80048dcc81944cccdfdc
Ulysse Carion <ulyssecarion@gmail.com>
2013-07-30 18:40:14 -0700

    Add a little thing to show how much time is left for an assignment

--- app/helpers/assignment_helper.rb
+++ app/helpers/assignment_helper.rb
@@ -1,2 +1,11 @@
 module AssignmentHelper
+  def time_left_message(assignment)
+    human_due_time = assignment.due_time.strftime("%I:%M%p %B %d")
+
+    if assignment.open?
+      "Closes at #{human_due_time} (#{time_ago_in_words(assignment.due_time)} from now)"
+    else
+      "Closed since #{human_due_time} (#{time_ago_in_words(assignment.due_time)} ago)"
+    end
+  end
 end
