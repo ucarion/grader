4a28713501eb76e0dfd8d2dccdc9e2c5c261777f
Ulysse Carion <ulyssecarion@gmail.com>
2013-08-03 14:21:03 -0700

    Add validations for the presence of author and assignment id to submissions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,7 @@
 class Submission < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   belongs_to :assignment
+
+  validates :author_id, presence: true
+  validates :assignment_id, presence: true
 end
