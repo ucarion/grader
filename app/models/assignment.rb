b3f205df7d7486f1a76b38140db8eab2a7e73621
Ulysse Carion <ulyssecarion@gmail.com>
2013-08-03 13:23:22 -0700

    By default, order assignments by ascending due_time

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,8 @@
 class Assignment < ActiveRecord::Base
   belongs_to :course
 
+  default_scope -> { order('due_time ASC') }
+
   validates :name, presence: true
   validates :description, presence: true
   validates :course_id, presence: true
