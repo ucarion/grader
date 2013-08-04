556584c3064bb25230224232fb8a3353cb66cbae
Ulysse Carion <ulyssecarion@gmail.com>
2013-08-04 14:20:25 -0700

    Add tests for showing a submission's source attachment

--- spec/requests/submissions_pages_spec.rb
+++ spec/requests/submissions_pages_spec.rb
@@ -8,11 +8,20 @@ describe "SubmissionsPages" do
     let(:course) { FactoryGirl.create(:course, teacher: teacher) }
     let(:assignment) { FactoryGirl.create(:assignment, course: course) }
     let(:student) { FactoryGirl.create(:student) }
-    let(:submission) { student.submissions.create!(assignment: assignment) }
+
+    let(:submission) do
+      student.submissions.create!(assignment: assignment,
+        source_code: File.new(Rails.root + 'spec/example_files/valid.rb'))
+    end
 
     before { visit submission_path(submission) }
 
     it { should have_content(student.name) }
     it { should have_content(assignment.name) }
+
+    it "should show the submission's source code" do
+      source = File.read(submission.source_code.path)
+      expect(page).to have_content(source)
+    end
   end
 end
