1c86849d3f972012cb0fc951f581886a84d5b842
Ulysse Carion <ulyssecarion@gmail.com>
2013-08-04 16:06:17 -0700

    Add a new + update action and view to submission, with errors displayed

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -2,4 +2,27 @@ class SubmissionsController < ApplicationController
   def show
     @submission = Submission.find(params[:id])
   end
+
+  def new
+    @assignment = Assignment.find(params[:assignment_id])
+    @submission = Submission.new
+  end
+
+  def create
+    @assignment = Assignment.find(params[:assignment_id])
+    @submission = @assignment.submissions.build(submission_params)
+
+    if @submission.save
+      flash[:success] = "Your submission was created successfully."
+      redirect_to @submission
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def submission_params
+    params.require(:submission).permit(:source_code, :author_id)
+  end
 end
