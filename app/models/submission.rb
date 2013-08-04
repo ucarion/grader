76b7cdaa9a987e4d6b4f7185506ef6908e5332da
Ulysse Carion <ulyssecarion@gmail.com>
2013-08-04 14:21:35 -0700

    Add and validate for attachment presence

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,6 +2,9 @@ class Submission < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   belongs_to :assignment
 
+  has_attached_file :source_code
+
   validates :author_id, presence: true
   validates :assignment_id, presence: true
+  validates_attachment :source_code, presence: true
 end
