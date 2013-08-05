5637e4d3e23cdc8b1e4259f1d0c52e0004e29808
Ulysse Carion <ulyssecarion@gmail.com>
2013-08-04 23:09:34 -0700

    Cascade-delete assignments on course deletion

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
   belongs_to :teacher, class_name: "User"
   has_and_belongs_to_many :students, class_name: "User"
-  has_many :assignments
+  has_many :assignments, dependent: :destroy
 
   validates :teacher_id, presence: true
   validates :name, presence: true
