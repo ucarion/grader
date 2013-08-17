7047d9b31012f0fb70a8a0781e82be45ba14cc70
Ulysse Carion <ulyssecarion@gmail.com>
2013-08-16 23:26:28 -0700

    Add validations for showing / grading submissions

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -3,6 +3,8 @@ class SubmissionsController < ApplicationController
   before_filter :check_assignment_still_open, only: [:new, :create]
   before_filter :check_enrolled_in_corresponding_course, only: [:new, :create]
   before_filter :check_is_own_submission_or_is_teacher, only: [:show]
+  before_filter :check_is_teacher, only: [:grade]
+  before_filter :check_is_teacher_for_index, only: [:index]
 
   def show
     @submission = Submission.find(params[:id])
@@ -29,10 +31,30 @@ class SubmissionsController < ApplicationController
     @assignment = Assignment.find(params[:assignment_id])
   end
 
+  def grade
+    @submission = Submission.find(params[:id])
+
+    if @submission.update_attributes(submission_grading_params)
+      flash[:success] = "The submission was updated successfully."
+      redirect_to @submission
+    else
+      redirect_to @submission, flash: { error: submission_error_message }
+    end
+  end
+
   private
 
   def submission_params
     params.require(:submission).permit(:source_code, :author_id)
+  end
+
+  def submission_grading_params
+    params.require(:submission).permit(:grade)
+  end
+
+  # only show the first one, because otherwise it looks weird in an alert
+  def submission_error_message
+    "Error with your update: #{@submission.errors.full_messages.first}"
   end
 
   def check_assignment_still_open
@@ -54,5 +76,18 @@ class SubmissionsController < ApplicationController
     unless current_user?(submission.author) || current_user?(submission.assignment.course.teacher)
       redirect_to root_path, notice: "You cannot view others' submissions" 
     end
+  end
+
+  def check_is_teacher
+    submission = Submission.find(params[:id])
+    teacher = submission.assignment.course.teacher
+    redirect_to root_path, notice: "You cannot grade this assignment." unless current_user?(teacher)
+  end
+
+  def check_is_teacher_for_index
+    assignment = Assignment.find(params[:assignment_id])
+    teacher = assignment.course.teacher
+
+    redirect_to root_path, notice: "You cannot view this page" unless current_user?(teacher)
   end
 end
