db7d9f9c07d5c553c23dc932ac3189e23e1155ae
Ulysse Carion <ulyssecarion@gmail.com>
2013-08-18 15:18:37 -0700

    Add a create route for comments

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ Grader::Application.routes.draw do
     end
   end
 
+  resources :comments, only: [:create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
