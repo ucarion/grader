80fd808d8467a73f729639f44c73f952761188f8
Ulysse Carion <ulyssecarion@gmail.com>
2013-08-21 20:41:41 -0700

    Add a function for getting a submisison's output

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -9,4 +9,22 @@ class Submission < ActiveRecord::Base
   validates :assignment_id, presence: true
   validates_attachment :source_code, presence: true
   validates :grade, numericality: { only_integer: true }, allow_blank: true
+
+  DOCKER_RUBY_IMAGE_ID = '8d7cd7b96168'
+
+  def execute_code
+    image = ruby_image
+
+    file_name = source_code_file_name
+    source = File.read(source_code.path).shellescape
+    cmd = [ "/bin/bash", "-c", "echo #{source} > #{file_name}; ruby #{file_name}"]
+
+    image.run(cmd).attach
+  end
+
+  def ruby_image
+    Docker::Image.all.each do |image|
+      return image if image.id.starts_with?(DOCKER_RUBY_IMAGE_ID)
+    end
+  end
 end
