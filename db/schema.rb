e2b1f4c084b464b57d5e365f796497cf144f9ae5
Ulysse Carion <ulyssecarion@gmail.com>
2013-08-24 11:19:10 -0700

    Add a status field to submissions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130824004452) do
+ActiveRecord::Schema.define(version: 20130824180828) do
 
   create_table "assignments", force: true do |t|
     t.string   "name"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20130824004452) do
     t.datetime "source_code_updated_at"
     t.integer  "grade"
     t.text     "output"
+    t.integer  "status"
   end
 
   create_table "users", force: true do |t|
