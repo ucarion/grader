c975f5d71f993bdb69d9ae2ba7af6f59c6b72cf0
Ulysse Carion <ulyssecarion@gmail.com>
2013-09-05 18:12:53 -0700

    Have course controller and view be aware of the language attribute

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -56,6 +56,6 @@ class CoursesController < ApplicationController
   private
 
   def course_params
-    params.require(:course).permit(:name, :description)
+    params.require(:course).permit(:name, :description, :language)
   end
 end
