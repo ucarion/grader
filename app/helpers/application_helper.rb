893c62bb30722767921442c79ab3a2badc943526
Ulysse Carion <ulyssecarion@gmail.com>
2013-12-26 00:20:56 -0800

    Move markdown logic into the ApplicationHelper, and deprecate link_with_icon.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,9 +8,28 @@ module ApplicationHelper
     end
   end
 
+  # TODO: get rid of this blasted method
   def link_with_icon(icon, body, url, html_options = {})
     link_to(url, html_options) do
       content_tag(:i, '', class: "icon-fixed-width icon-#{icon}") + " " + body
     end
+  end
+
+  def markdown(text)
+    markdown_parser.render(text).html_safe
+  end
+
+  private
+
+  def markdown_parser
+    renderer = CustomRenderers::HtmlWithPygments
+    opts = {
+      hard_wrap: true,
+      fenced_code_blocks: true,
+      autolinks: true,
+      disable_indented_code_blocks: true
+    }
+
+    Redcarpet::Markdown.new(renderer, opts)
   end
 end
--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,19 +1,2 @@
 module CoursesHelper
-  def markdown(text)
-    markdown_parser.render(text).html_safe
-  end
-
-  private
-
-  def markdown_parser
-    renderer = CustomRenderers::HtmlWithPygments
-    opts = {
-      hard_wrap: true,
-      fenced_code_blocks: true,
-      autolinks: true,
-      disable_indented_code_blocks: true
-    }
-
-    Redcarpet::Markdown.new(renderer, opts)
-  end
 end
