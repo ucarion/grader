81c6c51dfb4c2192f44af44fc287ee946052dfb9
Ulysse Carion <ulyssecarion@gmail.com>
2013-12-25 23:47:04 -0800

    Add gravatar and deletion-links stuff to UsersHelper.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,17 @@
 module UsersHelper
   def main_profile_picture(user)
-    image_tag(gravatar_image_url(user.email),
-      class: "img-rounded", id: "profile-picture")
+    profile_picture(user, class: "img-rounded", id: "profile-picture")
+  end
+
+  def profile_picture(user, opts = {})
+    opts[:class] ||= ""
+    opts[:class] += " gravatar"
+
+    image_tag(gravatar_image_url(user.email), opts)
+  end
+
+  def link_to_destroy(user)
+    link_to("delete", user, method: :delete,
+      data: { confirm: "Are you sure you want to delete #{user.name}?" })
   end
 end
