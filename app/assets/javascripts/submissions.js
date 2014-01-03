388ecbeb81a0c373497b8b90e8c0b1a2768d4c3b
Ulysse Carion <ulyssecarion@gmail.com>
2014-01-02 18:00:59 -0800

    Add a "back to list submissions" button for teachers.
    This is implemented using a Bootstrap popover and, as part of the transition
    away from Coffeescript, is implemented in vanilla Javascript.

--- app/assets/javascripts/submissions.js
+++ app/assets/javascripts/submissions.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+  if ($('#back_to_list_submissions').length) {
+    // the properties of the popover are in HTML data-* attributes already.
+    $('#back_to_list_submissions').popover();
+  }
+});
