1867abfa55db6cfc67d1098adbaf8be4b3387ed5
Ulysse Carion <ulyssecarion@gmail.com>
2014-02-19 17:55:20 -0800

    Add a javascript message indicating when the grace period will end.
    Perhaps this should be done from Rails over an AJAX request?

--- app/assets/javascripts/assignment.js
+++ app/assets/javascripts/assignment.js
@@ -4,4 +4,44 @@
 $(function() {
   $('.datepicker').datepicker();
   $('.code-field').autosize();
+
+  if ($('#grace-period').length) {
+    var days = [
+      'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+    ];
+
+    var months = [
+      'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+    ];
+
+    var updateGracePeriodMessage = function() {
+      var dueTimeField = $('#due-time').val();
+
+      var dueTimeParts = dueTimeField.split('/');
+      var year = parseInt(dueTimeParts[2], 10);
+      var month = parseInt(dueTimeParts[0], 10) - 1;
+      var day = parseInt(dueTimeParts[1], 10);
+
+      var dueTime = new Date(year, month, day);
+
+      var gracePeriod = parseInt($('#grace-period').val(), 10);
+      var milliSecsOfGracePeriod = 24 * 60 * 60 * 1000 * gracePeriod;
+
+      var endGrace = new Date(dueTime.getTime() + milliSecsOfGracePeriod);
+      var endGraceMsg = days[endGrace.getDay()] + ', ' +
+        months[endGrace.getMonth()] + ' ' + endGrace.getDate();
+
+      var fullMessage;
+
+      if (isNaN(endGrace.getTime())) {
+        fullMessage = '';
+      } else {
+        fullMessage = 'Grace period will end on: ' + endGraceMsg;
+      }
+
+      $('#grace-period-message').text(fullMessage);
+    };
+
+    $('#grace-period, #due-time').on('change keyup', updateGracePeriodMessage);
+  }
 });
