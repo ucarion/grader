3dab5deae9bf693922fa2673d46741fdd68be554
Ulysse Carion <ulyssecarion@gmail.com>
2014-02-25 22:14:23 -0800

    Add schema.rb
    Oops. Now I'm too lazy to bother fixing the commit log.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140221164631) do
+ActiveRecord::Schema.define(version: 20140226050551) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 20140221164631) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "teacher",                default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
