2de22e25b0f14b9d065fd823ea62d4f96d29c540
Ulysse Carion <ulyssecarion@gmail.com>
2014-03-07 16:38:35 -0800

    Add a few deploy tasks for Capistrano to execute.
    These tasks are not fully tested yet, but they should theoretically restart the
    server and check that filesystem privileges are set up correctly.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,25 @@ set :repo_url, "git@github.com:ulyssecarion/grader.git"
 set :scm, "git"
 set :scm_passphrase, ""
 set :ssh_options, { forward_agent: true }
+
+namespace :deploy do
+  namespace :check do
+    desc "Check that we can access everything"
+    task :write_permissions do
+      on roles(:all) do |host|
+        if test("[ -w #{fetch(:deploy_to)} ]")
+          info "#{fetch(:deploy_to)} is writable on #{host}"
+        else
+          error "#{fetch(:deploy_to)} is not writable on #{host}"
+        end
+      end
+    end
+  end
+
+  desc "Restart Passenger app"
+  task :restart do
+    run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  end
+end
+
+after "deploy", "deploy:restart"
