399eaa31d01780ac38d50d36de607941e274be39
Ulysse Carion <ulyssecarion@gmail.com>
2014-03-09 16:34:56 -0700

    Move deploy tasks into custom_tasks.cap and used shared folders.
    It turns out Capistrano was completely ignoring my tasks because I had them in
    the wrong file. For this I blame Capistrano's issue with documenting whether a
    feature is for Capistrano 2 or Capistrano 3. In this case, I was using v2 syntax
    when in fact I'm running v3.
    
    In the future, all tasks will need to be placed in lib/capistrano/tasks/*.cap.
    
    In addition, the public/system directory is explicitly symlinked. This will make
    user submissions persist across all deployments.

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,37 +7,8 @@ set :scm, "git"
 set :scm_passphrase, ""
 set :ssh_options, { forward_agent: true }
 
-namespace :deploy do
-  namespace :check do
-    desc "Check that we can access everything"
-    task :write_permissions do
-      on roles(:all) do |host|
-        if test("[ -w #{fetch(:deploy_to)} ]")
-          info "#{fetch(:deploy_to)} is writable on #{host}"
-        else
-          error "#{fetch(:deploy_to)} is not writable on #{host}"
-        end
-      end
-    end
-  end
-
-  desc "Restart Passenger app"
-  task :restart do
-    on roles(:app) do
-      tmp = File.join(current_path, 'tmp')
-      restart_txt = File.join(tmp, 'restart.txt')
-
-      execute "mkdir -p #{tmp}; touch #{restart_txt}"
-    end
-  end
-
-  desc "Ping the server to force Passenger to reload"
-  task :ping do
-    on roles(:app) do |server|
-      `curl #{server.hostname} > /dev/null 2>&1`
-    end
-  end
-end
+set :linked_dirs, %w{public/system}
 
 after "deploy", "deploy:restart"
 after "deploy", "deploy:ping"
+after "deploy", "delayed_job:restart"
