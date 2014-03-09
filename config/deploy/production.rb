0f061af481dfc8d7095f391d16e558b3d36da979
Ulysse Carion <ulyssecarion@gmail.com>
2014-03-09 16:33:55 -0700

    Add a Capistrano task for Delayed::Job.
    This involves adding "rails_env" as a parameter, which was done in
    production.rb.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,6 @@ set :stage, :production
 server "162.243.207.97", user: "deploy", roles: [:web, :app, :db, :workers]
 set :deploy_to, "/webapps/grader-cap"
 
+set :rails_env, :production
+
 set :branch, "master"
