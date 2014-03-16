824cbcfe6dda84e270411ad1c2a2d573263f55f2
Ulysse Carion <ulyssecarion@gmail.com>
2014-03-16 14:50:54 -0700

    Rename app server to getcurriculum.com

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :stage, :production
 
-server "162.243.207.97", user: "deploy", roles: [:web, :app, :db, :workers]
+server "getcurriculum.com", user: "deploy", roles: [:web, :app, :db, :workers]
 set :deploy_to, "/webapps/grader-cap"
 
 set :rails_env, :production
