e2bedc9941fb1e7a0cbdea787a172b84a0d33e6b
Ulysse Carion <ulyssecarion@gmail.com>
2014-03-21 12:42:30 -0700

    Add Cap tasks for Docker.
    docker:pull will pull the submission image from the registry, and docker:version
    will display information that will show if the daemon is running or not.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,11 +10,16 @@ set :ssh_options, { forward_agent: true }
 set :linked_dirs, %w{public/system}
 
 before "deploy", "delayed_job:stop"
+before "deploy", "docker:pull"
+
 after "deploy", "deploy:restart"
 after "deploy", "deploy:ping"
 after "deploy", "delayed_job:start"
+
 after "deploy", "deploy:check:write_permissions"
 after "deploy", "deploy:check:docker_group"
 after "deploy", "deploy:check:delayed_job_status"
+after "deploy", "docker:version"
+
 after "deploy:started", "figaro:setup"
 after "deploy:symlink:release", "figaro:symlink"
