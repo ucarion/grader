c07525e67bf677c53aa56b3c11f74c6b65761eb2
Ulysse Carion <ulyssecarion@gmail.com>
2014-04-10 19:30:43 -0700

    Refactor #diff_between.
    The old implementation was quite the nightmare.

--- app/helpers/assignment_helper.rb
+++ app/helpers/assignment_helper.rb
@@ -13,63 +13,76 @@ module AssignmentHelper
     time.strftime("%m/%d/%Y")
   end
 
+  # Generates a table showing the difference between two files.
   def diff_between(submisison_a, submisison_b)
-    def diff_line_number(line_number, should_show_number)
-      content_tag(:td, should_show_number ? line_number : "",
-        class: 'diff-line-number')
+
+    # Diffy returns a list of 'chunks', where insertions, deletions, and
+    # unchanged sequences of lines are grouped up. This method ungroups them so
+    # that they're easier to display line-by-line.
+    def diff_chunks(submisison_a, submisison_b)
+      source_a = File.read(submisison_a.main_file.code.path)
+      source_b = File.read(submisison_b.main_file.code.path)
+
+      diff = ::Diffy::Diff.new(source_a, source_b)
+      diff.each_chunk.map do |chunk|
+        first_char, lines = chunk[0], chunk[1..-1].split("\n")
+
+        lines.map { |line| first_char + line }
+      end.flatten
     end
 
-    source_a = File.read(submisison_a.main_file.code.path)
-    source_b = File.read(submisison_b.main_file.code.path)
-
-    diff = ::Diffy::Diff.new(source_a, source_b)
-    chunks = diff.each_chunk.map do |chunk|
-      first_char, lines = chunk[0], chunk[1..-1].split("\n")
-
-      lines.map { |line| first_char + line }
-    end.flatten
-
-    line_number_a = line_number_b = 0
-
-    chunks_as_html = chunks.reduce("".html_safe) do |acc, chunk|
-      should_show_a = should_show_b = false
-      line_class = ""
-
-      case chunk[0]
-      when '+'
-        line_class = 'insertion'
-
-        line_number_a += 1
-        should_show_a = true
-      when '-'
-        line_class = 'deletion'
-
-        line_number_b += 1
-        should_show_b = true
-      else
-        line_number_a += 1
-        line_number_b += 1
-        should_show_a = should_show_b = true
+    def chunks_as_html(chunks)
+      def diff_line(line_class, &block)
+        content_tag(:tr, class: line_class, &block)
       end
 
-      text_to_display = if chunk.start_with?(' ')
-        "&nbsp;"
-      else
-        ""
-      end.html_safe + chunk
+      def diff_line_number(line_number, should_show_number)
+        content_tag(:td, should_show_number ? line_number : "",
+          class: 'diff-line-number')
+      end
 
-      acc + content_tag(:tr,
-        diff_line_number(line_number_a, should_show_a) +
-        diff_line_number(line_number_b, should_show_b) +
+      def diff_line_code(text)
         content_tag(:td,
-          content_tag(:span, text_to_display, class: "diff-line")
-        ),
+          content_tag(:span, text, class: 'diff-line')
+        )
+      end
 
-        class: line_class
-      )
+      safe_empty = "".html_safe
+
+      # These are the line numbers that will be shown off to the left of the
+      # code. Because insertions and deletions only exist in one and not both
+      # files, these two values will not always be the same.
+      line_numbers = {a: 0, b: 0}
+
+      chunks.reduce(safe_empty) do |acc, chunk|
+        line_type = case chunk[0]
+        when '+'
+          :insertion
+        when '-'
+          :deletion
+        else
+          :unchanged
+        end.to_s.inquiry
+
+        # This is important because unchanged lines will have a leading space
+        # that browsers will not normally show.
+        chunk_to_html = chunk
+
+        # Both line numbers incremented when line_type is 'unchanged'
+        line_numbers[:a] += 1 unless line_type.deletion?
+        line_numbers[:b] += 1 unless line_type.insertion?
+
+        acc + diff_line(line_type.to_s) do
+          diff_line_number(line_numbers[:a], !line_type.deletion?) +
+          diff_line_number(line_numbers[:b], !line_type.insertion?) +
+          diff_line_code(chunk_to_html)
+        end
+      end
     end
 
-    content_tag(:table, chunks_as_html,
+    table_content = chunks_as_html(diff_chunks(submisison_a, submisison_b))
+
+    content_tag(:table, table_content,
       class: 'table table-bordered table-condensed table-diff')
   end
 
