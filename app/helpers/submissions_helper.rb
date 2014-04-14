f0899454dc9580980d0c5f93523101e03e570046
Ulysse Carion <ulyssecarion@gmail.com>
2014-04-13 18:52:30 -0700

    Use OO rather than cases in SubmissionsHelper.

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -1,27 +1,7 @@
 module SubmissionsHelper
-  def table_class_for_status(status)
-    case status
-    when Submission::Status::WAITING
-      ""
-    when Submission::Status::OUTPUT_CORRECT
-      "success"
-    when Submission::Status::OUTPUT_INCORRECT
-      "danger"
-    end
-  end
-
-  def status_label(submission)
-    color, text = "", ""
-    case submission.status
-    when Submission::Status::WAITING
-      color, text = "default", "Waiting"
-    when Submission::Status::OUTPUT_CORRECT
-      color, text = "success", "Tests passed"
-    when Submission::Status::OUTPUT_INCORRECT
-      color, text = "danger", "Tests failed"
-    end
-
-    content_tag(:label, text, class: "label label-#{color}")
+  def status_label(status)
+    content_tag(:label, status.text_message,
+      class: "label label-#{status.css_class}")
   end
 
   def sanitize_for_comparison(output)
