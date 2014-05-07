9a663875a0f1eed3801f07447aff90d634a2c2a5
Ulysse Carion <ulyssecarion@gmail.com>
2014-05-07 12:56:27 -0700

    Update the name of the git repo in deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, 'grader'
 
 set :pty, true
 
-set :repo_url, "git@github.com:ulyssecarion/grader.git"
+set :repo_url, "git@github.com:ucarion/grader.git"
 set :scm, "git"
 set :scm_passphrase, ""
 set :ssh_options, { forward_agent: true }
